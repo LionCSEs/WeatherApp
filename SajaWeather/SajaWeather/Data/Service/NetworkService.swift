273cb2a86158c2e74a0b2379818b226de3c72120
jiyoungsophia <sophiathedev@gmail.com>
2025-08-06 14:14:46 +0900

    Initial project setting

---
+++ SajaWeather/SajaWeather/Data/Service/NetworkService.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkService.swift
+//  SajaWeather
+//
+//  Created by Milou on 8/6/25.
+//
+
+import Foundation
---
+++ SajaWeather/SajaWeather/Scene/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SajaWeather
+//
+//  Created by Milou on 8/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
