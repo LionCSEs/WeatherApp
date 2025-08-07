44bcde0ffaa6deeda988df289cf3fdcd0e98adf2
jiyoungsophia <sophiathedev@gmail.com>
2025-08-07 17:38:34 +0900

    feat: #4 - Moya 사용을 위한 WeatherAPI TargetType 구현

---
+++ SajaWeather/SajaWeather/Data/Service/WeatherAPI.swift
@@ -0,0 +1,102 @@
+//
+//  WeatherAPI.swift
+//  SajaWeather
+//
+//  Created by Milou on 8/7/25.
+//
+
+import Foundation
+import Moya
+
+enum WeatherAPI {
+  case current(lat: Double, lon: Double)
+  case hourlyForecast(lat: Double, lon: Double)
+  case dailyForecast(lat: Double, lon: Double)
+  case airQuality(lat: Double, lon: Double)
+}
+
+extension WeatherAPI: TargetType {
+  var baseURL: URL {
+    return URL(string: "https://api.openweathermap.org/data/2.5")!
+  }
+  
+  var path: String {
+    switch self {
+    case .current:
+      return "/weather"
+    case .hourlyForecast:
+      return "/forecast/hourly"
+    case .dailyForecast:
+      return "/forecast/daily"
+    case .airQuality:
+      return "/air_pollution"
+    }
+  }
+  
+  var method: Moya.Method {
+    return .get
+  }
+  
+  var task: Moya.Task {
+    switch self {
+    case .current(let lat, let lon):
+      return .requestParameters(
+        parameters: [
+          "lat": lat,
+          "lon": lon,
+          "appid": apiKey,
+          "units": "metric", // 섭씨로 받아옴
+          "lang": "kr"
+        ],
+        encoding: URLEncoding.queryString
+      )
+      
+    case .hourlyForecast(let lat, let lon):
+      return .requestParameters(
+        parameters: [
+          "lat": lat,
+          "lon": lon,
+          "appid": apiKey,
+          "units": "metric",
+          "lang": "kr",
+          "cnt" : 48 // 내일까지
+        ],
+        encoding: URLEncoding.queryString
+      )
+      
+    case .dailyForecast(let lat, let lon):
+      return .requestParameters(
+        parameters: [
+          "lat": lat,
+          "lon": lon,
+          "appid": apiKey,
+          "units": "metric",
+          "lang": "kr",
+          "cnt" : 10
+        ],
+        encoding: URLEncoding.queryString
+      )
+      
+    case .airQuality(let lat, let lon):
+      return .requestParameters(
+        parameters: ["lat": lat,
+                     "lon": lon,
+                     "appid": apiKey
+                    ],
+        encoding: URLEncoding.queryString)
+    }
+  }
+  
+  var headers: [String : String]? {
+    return ["Content-Type": "application/json"]
+  }
+}
+
+extension WeatherAPI {
+  private var apiKey: String {
+    guard let key = Bundle.main.apiKey else {
+      fatalError()
+    }
+    return key
+  }
+}
