d030777cb1a0e58fc32d197e8c0c7186e226f8e0
jiyoungsophia <sophiathedev@gmail.com>
2025-08-11 17:12:10 +0900

    chore: #14 - 변수명 수정 및 주석 추가

--- SajaWeather/SajaWeather/Data/Service/LocationSearchService.swift
+++ SajaWeather/SajaWeather/Data/Service/LocationSearchService.swift
@@ -10,16 +10,18 @@ import MapKit
 import RxSwift
 import CoreLocation
 
-// MARK: - LocationSearchServiceType Protocol
 protocol LocationSearchServiceType {
+  /// "사직동" 입력 -> [종로구 사직동, 동래구 사직동 ...] 자동완성 제안
   func searchCompleter(query: String) -> Observable<[Location]>
+  
+  /// "종로구 사직동" 선택 -> 정확한 주소 및 좌표 반환
   func searchDetail(completion: MKLocalSearchCompletion) -> Single<Location>
 }
 
-// MARK: - LocationSearchService Implementation
 final class LocationSearchService: NSObject, LocationSearchServiceType {
+  
   private let completer = MKLocalSearchCompleter()
-  private var currentObserver: AnyObserver<[Location]>?
+  private var searchObserver: AnyObserver<[Location]>?
   
   override init() {
     super.init()
@@ -33,34 +35,37 @@ final class LocationSearchService: NSObject, LocationSearchServiceType {
   
   // MARK: - 자동완성 검색
   func searchCompleter(query: String) -> Observable<[Location]> {
+    
     return Observable.create { [weak self] observer in
       guard let self = self else {
         observer.onCompleted()
         return Disposables.create()
       }
-            
+      
+      // 빈 검색어 체크 (공백 제거 후 비어있으면)
       if query.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
         observer.onNext([])
         observer.onCompleted()
         return Disposables.create()
       }
       
-      // 현재 observer 저장
-      self.currentObserver = observer.asObserver()
+      // 현재 observer 저장 (나중에 결과 전달받음)
+      self.searchObserver = observer.asObserver()
       
       // 검색 시작
       self.completer.queryFragment = query
       
       return Disposables.create {
         self.completer.cancel()
-        self.currentObserver = nil
+        self.searchObserver = nil
       }
     }
-    .timeout(.seconds(10), scheduler: MainScheduler.instance)
+    .timeout(.seconds(5), scheduler: MainScheduler.instance)
   }
   
-  // MARK: - 선택된 completion의 상세 정보 검색
+  // MARK: - 선택된 위치에 대한 상세 정보 검색
   func searchDetail(completion: MKLocalSearchCompletion) -> Single<Location> {
+    
     return Single.create { observer in
       
       let request = MKLocalSearch.Request(completion: completion)
@@ -95,7 +100,9 @@ final class LocationSearchService: NSObject, LocationSearchServiceType {
 }
 
 // MARK: - MKLocalSearchCompleterDelegate
+
 extension LocationSearchService: MKLocalSearchCompleterDelegate {
+  // 자동완성 결과가 도착했을때 호출되는 메서드
   func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
     
     let locations = completer.results.map { completion in
@@ -107,19 +114,21 @@ extension LocationSearchService: MKLocalSearchCompleterDelegate {
       )
     }
     
-    currentObserver?.onNext(locations)
-    currentObserver?.onCompleted()
-    currentObserver = nil
+    searchObserver?.onNext(locations)
+    searchObserver?.onCompleted()
+    searchObserver = nil
   }
   
+  // 검색 중 에러가 발생했을때 호출되는 메서드
   func completer(_ completer: MKLocalSearchCompleter, didFailWithError error: Error) {
-    currentObserver?.onNext([])
-    currentObserver?.onCompleted()
-    currentObserver = nil
+    searchObserver?.onNext([])
+    searchObserver?.onCompleted()
+    searchObserver = nil
   }
 }
 
 // MARK: - LocationSearchError
+
 enum LocationSearchError: LocalizedError {
   case noResults
   case unknown
