3f9f88ce3088db04a203901e5605972c714f84a2
jiyoungsophia <sophiathedev@gmail.com>
2025-08-11 21:39:20 +0900

    feat: #14 - DelegateProxy 구현 및 적용

--- SajaWeather/SajaWeather/Data/Service/LocationSearchService.swift
+++ SajaWeather/SajaWeather/Data/Service/LocationSearchService.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import MapKit
+import RxCocoa
 import RxSwift
 import CoreLocation
 
@@ -29,57 +30,41 @@ final class LocationSearchService: NSObject, LocationSearchServiceType {
   }
   
   private func setupCompleter() {
-    completer.delegate = self
     completer.resultTypes = .address
   }
   
   // MARK: - 자동완성 검색
+  
   func searchCompleter(query: String) -> Observable<[Location]> {
     
-    return Observable.create { [weak self] observer in
-      guard let self = self else {
-        observer.onCompleted()
-        return Disposables.create()
-      }
-      
-      // 빈 검색어 체크 (공백 제거 후 비어있으면)
-      if query.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
-        observer.onNext([])
-        observer.onCompleted()
-        return Disposables.create()
-      }
-      
-      // 현재 observer 저장 (나중에 결과 전달받음)
-      self.searchObserver = observer.asObserver()
-      
-      // 검색 시작
-      self.completer.queryFragment = query
-      
-      return Disposables.create {
-        self.completer.cancel()
-        self.searchObserver = nil
-      }
+    if query.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
+      return Observable.just([])
     }
+    
+    return completer.rx.query(query)
+      .map { completions in
+        completions.map { completion in
+          Location(
+            title: completion.title,
+            subtitle: completion.subtitle,
+            fullAddress: "\(completion.subtitle) \(completion.title)",
+            coordinate: CLLocationCoordinate2D(latitude: 0, longitude: 0)
+          )
+        }
+      }
     .timeout(.seconds(5), scheduler: MainScheduler.instance)
+    .catch { error in
+      return Observable.just([])
+    }
   }
   
   // MARK: - 선택된 위치에 대한 상세 정보 검색
+  
   func searchDetail(completion: MKLocalSearchCompletion) -> Single<Location> {
-    
-    return Single.create { observer in
-      
-      let request = MKLocalSearch.Request(completion: completion)
-      let search = MKLocalSearch(request: request)
-      
-      search.start { response, error in
-        if let error = error {
-          observer(.failure(error))
-          return
-        }
-        
-        guard let mapItem = response?.mapItems.first else {
-          observer(.failure(LocationSearchError.noResults))
-          return
+    return completion.rx.localSearch()
+      .map { response in
+        guard let mapItem = response.mapItems.first else {
+          throw LocationSearchError.noResults
         }
         
         let location = Location(
@@ -88,42 +73,9 @@ final class LocationSearchService: NSObject, LocationSearchServiceType {
           fullAddress: mapItem.placemark.title ?? "\(completion.subtitle) \(completion.title)",
           coordinate: mapItem.placemark.coordinate
         )
-        
-        observer(.success(location))
+        return location
       }
-      
-      return Disposables.create {
-        search.cancel()
-      }
-    }
-  }
-}
-
-// MARK: - MKLocalSearchCompleterDelegate
-
-extension LocationSearchService: MKLocalSearchCompleterDelegate {
-  // 자동완성 결과가 도착했을때 호출되는 메서드
-  func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
-    
-    let locations = completer.results.map { completion in
-      Location(
-        title: completion.title,
-        subtitle: completion.subtitle,
-        fullAddress: "\(completion.subtitle) \(completion.title)",
-        coordinate: CLLocationCoordinate2D(latitude: 0, longitude: 0)
-      )
-    }
-    
-    searchObserver?.onNext(locations)
-    searchObserver?.onCompleted()
-    searchObserver = nil
-  }
-  
-  // 검색 중 에러가 발생했을때 호출되는 메서드
-  func completer(_ completer: MKLocalSearchCompleter, didFailWithError error: Error) {
-    searchObserver?.onNext([])
-    searchObserver?.onCompleted()
-    searchObserver = nil
+      .asSingle()
   }
 }
 
@@ -140,5 +92,70 @@ enum LocationSearchError: LocalizedError {
     case .unknown:
       return "검색 중 오류가 발생했습니다"
     }
+  }
+}
+
+extension Reactive where Base: MKLocalSearchCompleter {
+  var delegate: DelegateProxy<MKLocalSearchCompleter, MKLocalSearchCompleterDelegate> {
+    return RxLocalSearchCompleterDelegateProxy.proxy(for: base)
+  }
+
+  func setDelegate(_ delegate: MKLocalSearchCompleterDelegate) -> Disposable {
+    return RxLocalSearchCompleterDelegateProxy.installForwardDelegate(
+      delegate,
+      retainDelegate: false,
+      onProxyForObject: base
+    )
+  }
+
+  func query(_ query: String) -> Observable<[MKLocalSearchCompletion]> {
+    return delegate
+      .methodInvoked(#selector(MKLocalSearchCompleterDelegate.completerDidUpdateResults(_:)))
+      .compactMap { $0[0] as? MKLocalSearchCompleter }
+      .map(\.results)
+      .do(onSubscribe: { [base] in
+        base.queryFragment = query
+      }, onDispose: { [base] in
+        base.cancel()
+      })
+  }
+}
+
+extension Reactive where Base: MKLocalSearchCompletion {
+  func localSearch() -> Observable<MKLocalSearch.Response> {
+    return Observable.create { [base] observer in
+      let search = MKLocalSearch(request: MKLocalSearch.Request(completion: base))
+      search.start { response, error in
+        if let error = error {
+          observer.on(.error(error))
+        } else if let response = response {
+          observer.on(.next(response))
+          observer.on(.completed)
+        }
+      }
+      return Disposables.create {
+        search.cancel()
+      }
+    }
+  }
+}
+
+// 프록시 먼저 만들기
+final class RxLocalSearchCompleterDelegateProxy: DelegateProxy<MKLocalSearchCompleter, MKLocalSearchCompleterDelegate>, DelegateProxyType, MKLocalSearchCompleterDelegate {
+  static func registerKnownImplementations() {
+    register {
+      RxLocalSearchCompleterDelegateProxy(
+        parentObject: $0,
+        delegateProxy: RxLocalSearchCompleterDelegateProxy.self
+      )
+    }
+  }
+
+  static func currentDelegate(for object: MKLocalSearchCompleter) -> MKLocalSearchCompleterDelegate? {
+    return object.delegate
+  }
+
+  static func setCurrentDelegate(_ delegate: MKLocalSearchCompleterDelegate?, to object: MKLocalSearchCompleter) {
+    object.delegate = delegate
   }
 }
