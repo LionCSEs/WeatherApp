1359400ad978e0aa97f7b87b7909317f8242c184
Wooseong <thehousedone@gmail.com>
2025-08-13 02:53:04 +0900

    feat: #30-수정하다 잘못 반영된 부분 다시 수정

--- SajaWeather/SajaWeather/Scene/WeatherList/ListCell.swift
+++ SajaWeather/SajaWeather/Scene/WeatherList/ListCell.swift
@@ -113,7 +113,7 @@ class ListCell: UICollectionViewCell {
   }
   
   func configure(with data: CurrentWeather, tempUnit: TemperatureUnit) {
-    backgroundImageView.image = UIImage(named: weatherIcon(for: data.icon, isDayTime: data.isDayNow))
+    backgroundImageView.image = UIImage(named: data.backgroundStyle.imageName)
     currentTempLabel.text = tempUnit == .celsius ? "\(data.temperature)°C" : "\(data.temperature)°F"
     locationLabel.text = data.address.fullAddress
     detailTempLabel.text = "↑\(data.maxTemp)°↓\(data.minTemp)° · 체감 온도 \(data.feelsLikeTemp)°"
